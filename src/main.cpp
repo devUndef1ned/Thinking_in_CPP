06fbc42853101e59c157abfa5197a7d999c4ccc1
ogkuzmin <ogkuzmin@bk.ru>
2016-07-17 22:54:26 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,9 @@
-/*
- * main.cpp
- *
- *  Created on: 4 февр. 2016 г.
- *      Author: ќлег
- */
 
-#include "Chapter3/chapter3.h"
-#include "Chapter4/chapter4.h"
-#include "Chapter5/chapter5.h"
-#include "Chapter6/chapter6.h"
-#include "Chapter7/chapter7.h"
-#include "Chapter8/chapter8.h"
+#include "Chapter10/chapter10.h"
 
 int main(){
 
-	exc8_7();
+	exc10_1();
 
 	return 0;
 }
